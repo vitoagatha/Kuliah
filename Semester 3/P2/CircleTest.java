adffec2d50c1bad5a6c762481a1c2b3ab7db0dcd
vito-agatha <128238004+vitoagatha@users.noreply.github.com>
2023-09-13 13:59:04 +0700

    Delete Semester 3/P2/CircleTest.java

--- Semester 3/P2/CircleTest.java
+++
@@ -1,9 +0,0 @@
-public class CircleTest {
-    public static void main(String[] args) {
-
-        Circle circle1 = new Circle();
-        circle1.radius = 10;
-        
-        System.out.println(circle1.getArea());
-    }
-}
