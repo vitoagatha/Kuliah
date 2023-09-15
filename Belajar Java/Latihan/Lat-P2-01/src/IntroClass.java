bcb5ef1e750afa265294d857724f4e51ae8bc2a6
vitoagatha <vitoagatha593@gmail.com>
2023-09-16 00:24:37 +0700

    first commit

--- Belajar Java/Latihan/Lat-P2-01/src/IntroClass.java
+++ Belajar Java/Latihan/Lat-P2-01/src/IntroClass.java
@@ -1,2 +1,19 @@
 public class IntroClass {
+    public static void main(String[] args) {
+        Murid budi = new Murid();
+
+        budi.nama = "Budi Tabudy";
+        budi.nim = 123;
+        budi.nilai = 85;
+
+        System.out.println("Nama: " + budi.nama);
+        System.out.println("Nilai: " + budi.nilai);
+        System.out.println("Nim: " + budi.nim);
+    }
+}
+
+class Murid {
+    String nama;
+    int nim;
+    int nilai;
 }
